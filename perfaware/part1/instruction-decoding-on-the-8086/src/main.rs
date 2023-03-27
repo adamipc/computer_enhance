a8fcfca4c032061b56dd89b884e95ca60395968f
Adam Wendt <adam@awendtconsulting.com>
2023-03-27 12:05:46 -0700

    decoding mov instruction complete

--- perfaware/part1/instruction-decoding-on-the-8086/src/main.rs
+++ perfaware/part1/instruction-decoding-on-the-8086/src/main.rs
@@ -3,11 +3,16 @@ use std::fs::File;
 use std::io;
 use std::io::Read;
 
-const OPCODES: [(u8, (&str, usize)); 4] = [
-    (0b10001000, ("mov-byte-reg-as-src", 1)),
-    (0b10001001, ("mov-word-reg-as-src", 1)),
-    (0b10001010, ("mov-byte-reg-as-dest", 1)),
-    (0b10001011, ("mov-word-reg-as-dest", 1)),
+const OPCODES: [(u8, (&str, usize)); 1] = [(0b10001000, ("mov", 1))];
+const REG_ENCODING: [(u8, (&str, &str)); 8] = [
+    (0x0, ("al", "ax")),
+    (0x1, ("cl", "cx")),
+    (0x2, ("dl", "dx")),
+    (0x3, ("bl", "bx")),
+    (0x4, ("ah", "sp")),
+    (0x5, ("ch", "bp")),
+    (0x6, ("dh", "si")),
+    (0x7, ("bh", "di")),
 ];
 
 fn main() -> io::Result<()> {
@@ -28,27 +33,58 @@ fn main() -> io::Result<()> {
     println!("\nbits 16\n");
     let mut e = buffer.iter().enumerate();
     while let Some((i, byte)) = e.next() {
-        println!("; {i}: {byte:#b}");
+        //println!("; {i}: {byte:#b}");
         let (instruction, datalen) = get_instruction(*byte).unwrap();
-        println!("Got instruction: {instruction}. Need to read {datalen} bytes of data.");
+        //println!("; Got instruction: {instruction}. Need to read {datalen} bytes of data.");
+        let reg_is_destination = byte & 0x2 == 0x2;
+        let operates_on_word = byte & 0x1 == 0x1;
+        //println!("; REG is destination: {reg_is_destination}, Operates on word: {operates_on_word}");
         let mut data = Vec::new();
         for _ in [0..datalen] {
             match e.next() {
                 Some((i, byte)) => {
-                    println!("; {i}: {byte:#b}");
+                    //println!("; data {i}: {byte:#b}");
                     data.push(byte)
                 }
                 None => panic!("Couldn't read enough data."),
             }
+        }
+
+        match instruction {
+            "mov" => {
+                let reg = get_reg_encoding((*data[0] & 0b00111000) >> 3, operates_on_word).unwrap();
+                let rm = get_reg_encoding(*data[0] & 0b00000111, operates_on_word).unwrap();
+                //println!("; Got REG/RM registers: {reg}/{rm}");
+                if reg_is_destination {
+                    println!("mov {reg}, {rm}");
+                } else {
+                    println!("mov {rm}, {reg}");
+                }
+            }
+            &_ => todo!(),
         }
     }
 
     Ok(())
 }
 
+fn get_reg_encoding(byte: u8, operates_on_word: bool) -> Option<&'static str> {
+    for (reg_value, (reg_byte, reg_word)) in REG_ENCODING {
+        if byte == reg_value {
+            if operates_on_word {
+                return Some(reg_word);
+            } else {
+                return Some(reg_byte);
+            }
+        }
+    }
+
+    None
+}
+
 fn get_instruction(byte: u8) -> Option<(&'static str, usize)> {
-    for (mask, instruction) in OPCODES {
-        if mask == byte {
+    for (opcode, instruction) in OPCODES {
+        if (byte & 0b11111100) == opcode {
             return Some(instruction);
         }
     }
